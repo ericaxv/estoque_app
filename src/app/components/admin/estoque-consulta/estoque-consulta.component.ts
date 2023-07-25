e3c1772675f04fd5cda5a9874329e29bd00de04c
Erica Xavier <ericaxav@gmail.com>
2023-07-24 22:16:32 -0300

    Consulta de Estoques

--- src/app/components/admin/estoque-consulta/estoque-consulta.component.ts
+++ src/app/components/admin/estoque-consulta/estoque-consulta.component.ts
@@ -1,39 +1,48 @@
-import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { getAllEstoque } from 'src/app/services/estoques/estoques.service';
+import { EstoqueGetResponseModel } from 'src/app/models/estoques/estoque-get.response.model';
 
 @Component({
   selector: 'app-estoque-consulta',
   templateUrl: './estoque-consulta.component.html',
   styleUrls: ['./estoque-consulta.component.css']
 })
-export class EstoqueConsultaComponent {
-  columnNames: string[] = ['nome', 'descricao', 'datacriacao',
-    'operacoes'];
-  //dados do grid
-  dataSource = new MatTableDataSource<DataSourceModel>([
-    {
-      nome: 'Estoque A', descricao: 'Estoque Teste',
-      datacriacao: new Date()
-    },
-    {
-      nome: 'Estoque B', descricao: 'Estoque Teste',
-      datacriacao: new Date()
-    },
-    {
-      nome: 'Estoque C', descricao: 'Estoque Teste',
-      datacriacao: new Date()
-    },
-    {
-      nome: 'Estoque D', descricao: 'Estoque Teste',
-      datacriacao: new Date()
-    },
-  ]);
-
+export class EstoqueConsultaComponent implements OnInit {
   //construtor
   constructor(
-    private router: Router
-  ) {
+    private router: Router,
+    private spinnerService: NgxSpinnerService
+  ) {}
+
+  
+  //atributos
+  columnNames: string[] = ['nome', 'descricao', 'datacriacao','operacoes'];
+  //dados do grid
+  dataSource = new MatTableDataSource<DataSourceModel>();
+
+  ngOnInit(): void {
+    this.spinnerService.show();
+    getAllEstoque().subscribe({
+      next: (data) => {
+        const dados: any[] = [];
+        data.forEach((item: EstoqueGetResponseModel) => {
+          dados.push({
+            nome: item.nome,
+            descricao: item.descricao,
+            datacriacao: item.dataHoraCadastro
+          })
+        });
+        this.dataSource.data = dados;
+      },
+      error: (e) => {
+        console.log(e.response.data);
+      }
+    }).add(() => {
+      this.spinnerService.hide();
+    });
   }
   //função para capturar o evento de exclusão
   onDelete(id: number): void {
